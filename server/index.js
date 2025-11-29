4caaafbe1cf205a2b8cbd9d0f4623e1bb5516ddc
kiranpoudel <kiranpoudel160@gmail.com>
2025-11-29 15:25:16 +0545

    new working port

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 const http = require("http");
-require("dotenv").config()
-const app = require("./src/config/express.config")
+require("dotenv").config();
+const app = require("./src/config/express.config");
 const { initSocket } = require("./src/config/socket");
-const httpServer = http.createServer(app);
 
 const PORT = process.env.PORT || 9005;
-const host = "127.0.0.1";
+
+const HOST = "0.0.0.0";
+
+const httpServer = http.createServer(app);
 
 initSocket(httpServer);
-httpServer.listen(PORT,host ,(err) => {
+
+
+httpServer.listen(PORT, HOST, (err) => {
     if (!err) {
-        console.log("server is running on port: ", PORT);
-        console.log("press CTRL+C to terminate server...");
-        console.info(`URL:http://${host}:${PORT}`);
+        console.log("***** Server is running *****");
+        console.log("Port:", PORT);
+        console.log("Press CTRL+C to terminate server...");
+        console.info(`URL: http://${HOST}:${PORT}`);
+    } else {
+        console.error("Server failed to start:", err);
     }
-
 });
-
